5d66d866ea5adaab9aa57c0bfe19ae0f9f26af09
tinyian <81102958+tinyian@users.noreply.github.com>
2024-10-28 09:15:05 +0000

    REACT App Setup

--- appt-hub/src/App.tsx
+++ appt-hub/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface MyButtonProps {
+  /** The text to display site header */
+  header: string;
+  /** The text to display inside the button */
+  btnTitle: string;
+  /** Whether the button can be interacted with */
+  disabled: boolean;
 }
 
-export default App
+function MyHeader({header}: MyButtonProps) {
+  return (
+    <h1>{header}</h1>
+  );
+}
+
+function MyButton({btnTitle, disabled }: MyButtonProps) {
+  return (
+    <button disabled={disabled}>{btnTitle}</button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <MyHeader header="Better Appointments"/>
+      <MyButton btnTitle="I'm a disabled button" disabled={true}/>
+      <MyButton btnTitle="I'm an enabled button" disabled={false}/>
+    </div>
+  );
+}
